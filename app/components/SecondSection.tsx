a955911780a08755684f7a612ca414493342bb1d
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-09-28 17:55:03 +0500

    first commit

---
+++ app/components/SecondSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface SecondSectionProps{
+
+}
+
+const SecondSection: React.FC<SecondSectionProps> = () => {
+  return (
+    <section className='flex justify-between'>
+        <div>
+            
+        </div>
+    </section>
+  )
+}
+
+export default SecondSection
