5890ade2d939289ee39ba74c5a9d347b577c5207
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-10-14 17:14:51 +0500

    animation

--- app/components/FifthSection.tsx
+++ app/components/FifthSection.tsx
@@ -1,19 +1,30 @@
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 interface FifthSectionSectionProps {}
 
 const FifthSection: React.FC<FifthSectionSectionProps> = () => {
     return (
         <section className="mt-20">
-            <h1 className="font-bold gap-4 text-center leading-tight text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-3xl tracking- font-['lepka']">
+            <motion.h1
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0, ease: "backOut", duration: 2 }}
+                className="font-bold gap-4 text-center leading-tight text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-3xl tracking- font-['lepka']"
+            >
                 SOME OF OUR{" "}
                 <span className='text-[#1C8BAD] font-["lepka"]'>
                     Ice cream lovers
                 </span>
-            </h1>
+            </motion.h1>
             <div className="grid grid-cols-3 max-xl:grid-cols-2 max-md:grid-cols-1 gap-14 max-sm:gap-7 my-6">
-                <div className="p-7 shadow-2xl rounded-2xl">
+                <motion.div
+                    initial={{ opacity: 0, x: "-100%" }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0, ease: "backOut", duration: 2 }}
+                    className="p-7 shadow-2xl rounded-2xl"
+                >
                     <div className="flex items-center justify-between">
                         <div>
                             <p className="text-3xl max-xl:text-2xl font-semibold">
@@ -32,7 +43,7 @@ const FifthSection: React.FC<FifthSectionSectionProps> = () => {
                             alt=""
                             width={70}
                             height={70}
-                            className={'max-lg:w-[50px] max-h-[50px]'}
+                            className={"max-lg:w-[50px] max-h-[50px]"}
                         />
                     </div>
                     <p className="mt-10 text-xl max-lg:text-lg font-semibold">
@@ -40,8 +51,13 @@ const FifthSection: React.FC<FifthSectionSectionProps> = () => {
                         sed do eiusmod tempor incididunt ut labore et dolore
                         magna aliqua.
                     </p>
-                </div>
-                <div className="p-7 shadow-2xl rounded-2xl ">
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, x: "100%" }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0, ease: "backOut", duration: 2 }}
+                    className="p-7 shadow-2xl rounded-2xl "
+                >
                     <div className="flex items-center justify-between">
                         <div>
                             <p className="text-3xl max-xl:text-2xl font-semibold">
@@ -60,7 +76,9 @@ const FifthSection: React.FC<FifthSectionSectionProps> = () => {
                             alt=""
                             width={70}
                             height={70}
-                            className={'pointer-events-none max-lg:w-[50px] max-h-[50px]'}
+                            className={
+                                "pointer-events-none max-lg:w-[50px] max-h-[50px]"
+                            }
                         />
                     </div>
                     <p className="mt-10 text-xl max-lg:text-lg font-semibold">
@@ -68,8 +86,13 @@ const FifthSection: React.FC<FifthSectionSectionProps> = () => {
                         sed do eiusmod tempor incididunt ut labore et dolore
                         magna aliqua.{" "}
                     </p>
-                </div>
-                <div className="p-7 shadow-2xl rounded-2xl row-start-2 row-end-3 col-start-2 col-end-3 max-md:row-start-4 max-md:row-end-5 max-md:col-span-1">
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, y: "100%" }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 0, ease: "backOut", duration: 2 }}
+                    className="p-7 shadow-2xl rounded-2xl row-start-2 row-end-3 col-start-2 col-end-3 max-md:row-start-4 max-md:row-end-5 max-md:col-span-1"
+                >
                     <div className="flex items-center justify-between">
                         <div>
                             <p className="text-3xl max-xl:text-2xl font-semibold">
@@ -88,7 +111,9 @@ const FifthSection: React.FC<FifthSectionSectionProps> = () => {
                             alt=""
                             width={70}
                             height={70}
-                            className={'max-lg:w-[50px] max-h-[50px] pointer-events-none'}
+                            className={
+                                "max-lg:w-[50px] max-h-[50px] pointer-events-none"
+                            }
                         />
                     </div>
                     <p className="mt-10 text-xl max-lg:text-lg font-semibold">
@@ -96,8 +121,13 @@ const FifthSection: React.FC<FifthSectionSectionProps> = () => {
                         sed do eiusmod tempor incididunt ut labore et dolore
                         magna aliqua.{" "}
                     </p>
-                </div>
-                <div className="flex items-center justify-center col-start-2 col-end-3 row-start-1 row-end-2 max-xl:col-span-1 ">
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, y: -50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 0, ease: "backOut", duration: 2 }}
+                    className="flex items-center justify-center col-start-2 col-end-3 row-start-1 row-end-2 max-xl:col-span-1 "
+                >
                     <Image
                         src={"/image/section2-ice-cream-child.svg"}
                         alt=""
@@ -105,7 +135,7 @@ const FifthSection: React.FC<FifthSectionSectionProps> = () => {
                         height={100}
                         className="h-full w-[70%] max-md:w-[60%] max-sm:w-[80%] pointer-events-none"
                     />
-                </div>
+                </motion.div>
             </div>
         </section>
     );
--- app/components/FourthSection.tsx
+++ app/components/FourthSection.tsx
@@ -1,68 +1,137 @@
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 
 interface FourthSectionProps {}
 
+const cardVariants: Variants = {
+    offscreen: {
+        y: 300,
+    },
+    onscreen: {
+        y: 50,
+        rotate: -10,
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 0.8,
+        },
+    },
+};
+
 const FourthSection: React.FC<FourthSectionProps> = () => {
     return (
         <section className="mt-20">
-            <div className="bg-[#53C9ED] flex max-xl:flex-col justify-center items-center p-20 max-sm:p-16 rounded-[80px] max-md:rounded-[50px] max-sm:rounded-[30px] relative">
-                <Image
-                    src={"/image/fourthSectionVector.svg"}
-                    alt=""
-                    width={170}
-                    height={170}
+            <div className="bg-[#53C9ED] flex max-xl:flex-col justify-center items-center p-20 max-sm:p-16 rounded-[80px] max-md:rounded-[50px] max-sm:rounded-[30px] relative overflow-hidden">
+                <motion.div
+                    initial={{ x: "-100%" }}
+                    whileInView={{ x: "0" }}
+                    transition={{ ease: "backOut", duration: 2 }}
                     className="pointer-events-none absolute top-10 left-10 max-sm:w-[130px] max-[500px]:w-[100px] max-sm:left-5 max-sm:top-5"
-                />
-                <Image
-                    src={"/image/fourthSectionVector.svg"}
-                    alt=""
-                    width={170}
-                    height={170}
-                    className="pointer-events-none absolute top-10 right-10 rotate-90 max-sm:w-[130px] max-[500px]:w-[100px] max-sm:right-5 max-sm:top-5"
-                />
-                <Image
-                    src={"/image/fourthSectionVector.svg"}
-                    alt=""
-                    width={170}
-                    height={170}
-                    className="pointer-events-none absolute bottom-10 right-10 rotate-180 max-sm:w-[130px] max-[500px]:w-[100px] max-sm:right-5 max-sm:bottom-5"
-                />
-                <Image
-                    src={"/image/fourthSectionVector.svg"}
-                    alt=""
-                    width={170}
-                    height={170}
-                    className="pointer-events-none absolute bottom-10 left-10 -rotate-90 max-sm:w-[130px] max-[500px]:w-[100px] max-sm:left-5 max-sm:bottom-5"
-                />
+                >
+                    <Image
+                        src={"/image/fourthSectionVector.svg"}
+                        alt=""
+                        width={170}
+                        height={170}
+                    />
+                </motion.div>
 
-                <div className="w-full flex items-center justify-center">
+                <motion.div
+                    initial={{ x: "100%" }}
+                    whileInView={{ x: "0" }}
+                    transition={{ ease: "backOut", duration: 2 }}
+                    className="pointer-events-none absolute top-10 right-10 max-sm:w-[130px] max-[500px]:w-[100px] max-sm:right-5 max-sm:top-5"
+                >
+                    <Image
+                        src={"/image/fourthSectionVector.svg"}
+                        alt=""
+                        width={170}
+                        height={170}
+                        className="rotate-90"
+                    />
+                </motion.div>
+
+                <motion.div
+                    initial={{ x: "100%" }}
+                    whileInView={{ x: "0" }}
+                    transition={{ ease: "backOut", duration: 2 }}
+                    className="pointer-events-none absolute bottom-10 right-10 max-sm:w-[130px] max-[500px]:w-[100px] max-sm:right-5 max-sm:bottom-5"
+                >
+                    <Image
+                        src={"/image/fourthSectionVector.svg"}
+                        alt=""
+                        width={170}
+                        height={170}
+                        className={"rotate-180"}
+                    />
+                </motion.div>
+
+                <motion.div
+                    initial={{ x: "-100%" }}
+                    whileInView={{ x: "0" }}
+                    transition={{ ease: "backOut", duration: 2 }}
+                    className="pointer-events-none absolute bottom-10 left-10 max-sm:w-[130px] max-[500px]:w-[100px] max-sm:left-5 max-sm:bottom-5"
+                >
+                    <Image
+                        src={"/image/fourthSectionVector.svg"}
+                        alt=""
+                        width={170}
+                        height={170}
+                        className={"-rotate-90"}
+                    />
+                </motion.div>
+
+                <motion.div
+                    initial={{ x: "-100%" }}
+                    whileInView={{ x: "0" }}
+                    transition={{ delay: 1, ease: "backOut", duration: 3 }}
+                    className="w-full flex items-center justify-center"
+                >
                     <Image
                         src={"/image/firthSection-Ice-cream.svg"}
                         alt=""
                         width={500}
                         height={500}
-						className="pointer-events-none"
+                        className="pointer-events-none"
                     />
-                </div>
+                </motion.div>
 
                 <div className="w-full max-xl:w-3/4 max-sm:w-[100%]">
-                    <p className='text-5xl max-xl:text-4xl max-md:text-3xl max-sm:text-2xl text-white font-["lepka"]'>
+                    <motion.p
+                        initial={{ x: "110%", opacity:0}}
+                        whileInView={{ x: "0", opacity:1 }}
+                        transition={{ delay: 1, ease: "backOut", duration: 2 }}
+                        className='text-5xl max-xl:text-4xl max-md:text-3xl max-sm:text-2xl text-white font-["lepka"]'
+                    >
                         The result is a smooth and semi-solid foam
-                    </p>
-                    <p className="text-2xl max-xl:text-xl max-sm:text-base mt-5 font-semibold max-sm:mt-3">
+                    </motion.p>
+                    <motion.p
+                        initial={{ x: "110%", opacity:0 }}
+                        whileInView={{ x:"0", opacity:1 }}
+                        transition={{
+                            delay: 1.5,
+                            ease: "backOut",
+                            duration: 2,
+                        }}
+                        className="text-2xl max-xl:text-xl max-sm:text-base mt-5 font-semibold max-sm:mt-3"
+                    >
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore
                         magna aliqua. Ut enim ad minim veniam, quis nostrud
                         exercitation ullamco laboris nisi ut aliquip ex ea
                         commodo consequat.{" "}
-                    </p>
-                    <div className="flex max-md:flex-col items-center max-md:items-start gap-10 max-sm:gap-5 mt-5">
+                    </motion.p>
+                    <motion.div
+                        initial={{ x: "110%", opacity:0 }}
+                        whileInView={{ x: "0", opacity:1 }}
+                        transition={{ delay: 2, ease: "backOut", duration: 2 }}
+                        className="flex max-md:flex-col items-center max-md:items-start gap-10 max-sm:gap-5 mt-5"
+                    >
                         <button className="bg-[#014EA1] py-5 px-10 max-sm:py-3 max-sm:px-5 text-white text-xl max-sm:text-base rounded-full">
                             Купить сейчас
                         </button>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
